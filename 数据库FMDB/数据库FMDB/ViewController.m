54ab8981754f0cdba7bf7e08255edb4a42a36897
fishTao <tao_0504@sina.com>
2016-03-03 11:25:09 +0800

    commit

--- 数据库FMDB/数据库FMDB/ViewController.m
+++ 数据库FMDB/数据库FMDB/ViewController.m
@@ -11,6 +11,9 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *namefiled;
 @property (weak, nonatomic) IBOutlet UITextField *priceField;
+//数据库实例对象
+@property (nonatomic,assign) sqlite3 *db;
+
 - (IBAction)insert;
 
 @end
@@ -20,19 +23,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //打开数据库
+    //打开数据库.连接数据库
    NSString *filename = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject]stringByAppendingString:@"shops.sqlite"];
+    //如果数据库文件不存在，系统会自己创建数据库，并初始化。
     
+  
+    int status = sqlite3_open(filename.UTF8String, &_db);
+    if (status == SQLITE_OK) {//打开数据库成功
+        NSLog(@"打开数据库成功");
+        const char *sql = "CREATE TABLE IF NOT EXISTS T_shop (id intager PRIMARY KEY,name text NOT NULL,price,real)";
+        char *errmsg = NULL;
+        sqlite3_exec(self.db, sql, NULL, NULL, &errmsg);
+        if (errmsg) {
+            NSLog(@"创表失败---%s",errmsg);
+        }
+        
+    }else{//打开失败
+        NSLog(@"打开数据库失败");
     
+    }
     
 //
 //    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (IBAction)inside {
-    
-    
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -40,5 +54,8 @@
 }
 
 - (IBAction)insert {
+    NSString *sql = [NSString stringWithFormat:@"INSERT INTO t_shops(name,price) VALUES('%@',%f);",self.namefiled.text,self.priceField.text.doubleValue];
+    sqlite3_exec(self.db, sql.UTF8String, NULL, NULL, NULL);
+
 }
 @end
